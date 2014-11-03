72d8ff69718f66bfff2fc6d524030462d1d33e7c
Francis Stephens <francisstephens@gmail.com>
2014-11-03 14:42:38 +0000

    Adding source ~/.profile to get $GOLANG value

--- all.sh
+++ all.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source ~/.profile
+echo $GOLANG
 wget https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-1.9.14.zip
 unzip go_appengine_sdk_linux_amd64-1.9.14.zip -d $GOLANG
 rm go_appengine_sdk_linux_amd64-1.9.14.zip
