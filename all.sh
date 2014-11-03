9f9072f21374a79d08112a5d6a54d8a0a1b6ece8
Francis Stephens <francisstephens@gmail.com>
2014-11-03 13:46:12 +0000

    Script downloads and sets up Go App-Engine

---
+++ all.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-1.9.14.zip
+unzip go_appengine_sdk_linux_amd64-1.9.14.zip -d $GOLANG
+rm go_appengine_sdk_linux_amd64-1.9.14.zip
+echo "export PATH=$PATH:$GOLANG/go_appengine" >> ~/.profile
