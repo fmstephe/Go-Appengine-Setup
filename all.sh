d34ce39ae93c5087a3b5c8b5184e82d141225170
Francis Stephens <francisstephens@gmail.com>
2014-11-26 10:49:23 +0000

    Upgrade SDK to version 1.9.15

--- all.sh
+++ all.sh
@@ -2,7 +2,7 @@
 
 source ~/.profile
 echo $GOLANG
-wget https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-1.9.14.zip
-unzip go_appengine_sdk_linux_amd64-1.9.14.zip -d $GOLANG
-rm go_appengine_sdk_linux_amd64-1.9.14.zip
+wget https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-1.9.15.zip
+unzip go_appengine_sdk_linux_amd64-1.9.15.zip -d $GOLANG
+rm go_appengine_sdk_linux_amd64-1.9.15.zip
 echo "export PATH=$PATH:$GOLANG/go_appengine" >> ~/.profile
